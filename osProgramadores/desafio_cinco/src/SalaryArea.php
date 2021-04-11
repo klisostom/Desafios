23c0f07a042498183bd7d8293a8a9c2f31b1820d
Klisóstom <klis@pm.me>
2021-04-11 10:53:34 -0300

    feat(desafio_cinco): nova classe

--- osProgramadores/desafio_cinco/src/SalaryArea.php
+++ osProgramadores/desafio_cinco/src/SalaryArea.php
@@ -2,8 +2,33 @@
 
 namespace App;
 
-class Salary implements SalaryInterface
+require_once('SalaryInterface.php');
+
+use App\SalaryInterface;
+
+class SalaryArea implements SalaryInterface
 {
-    
+    public function bigger(): array
+    {
+        return [];
+    }
+    public function smaller(): array
+    {
+        return [];
+    }
+    public function average(): array|float
+    {
+        return [];
+    }
+
+    public function highestsSalariesByArea(): array
+    {
+        return [1];
+    }
+
+    public function highestsSalariesPrintedByArea(): string
+    {
+        return '';
+    }
 }
 
