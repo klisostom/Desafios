385d4fae26e5412acbe8d7874e8e39f13f260e1b
Klisóstom <klis@pm.me>
2021-04-11 17:03:14 -0300

    feat(desafio_cinco): adição do construtor e implementação da função bigger

--- osProgramadores/desafio_cinco/src/SalaryArea.php
+++ osProgramadores/desafio_cinco/src/SalaryArea.php
@@ -3,32 +3,55 @@
 namespace App;
 
 require_once('SalaryInterface.php');
+require_once('EmployeeTrait.php');
 
+use EmployeeTrait;
 use App\SalaryInterface;
 
 class SalaryArea implements SalaryInterface
 {
+    use EmployeeTrait;
+
+    public function __construct(protected mixed $employees = [])
+    {
+        $this->employees = (count($employees) === 0) ?
+            $this->employees() :
+            collect($employees);
+    }
+
     public function bigger(): array
     {
-        return [];
+        return $this->calculateSalary($this->employees)
+            ->groupBy('area')
+            ->map(function ($area) {
+                return $area->groupBy('salario')
+                        ->sortKeysDesc()
+                        ->first()
+                        ->map(function ($employee) {
+                            return "area_max|{$employee['area']}|{$employee['nome']} {$employee['sobrenome']}|" .
+                                number_format($employee['salario'], 2, '.', '');
+                        });
+            })->toArray();
     }
+
     public function smaller(): array
     {
         return [];
     }
+
     public function average(): array|float
     {
         return [];
     }
 
-    public function highestsSalariesByArea(): array
+    public function highestsSalariesByArea(): mixed
     {
-        return [1];
+        return $this->bigger();
     }
 
-    public function highestsSalariesPrintedByArea(): string
+    public function highestsSalariesPrintedByArea(): mixed
     {
-        return '';
+        return $this->highestsSalariesByArea();
     }
 }
 
