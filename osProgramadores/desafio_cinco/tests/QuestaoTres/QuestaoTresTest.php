62bac938b60e961ba6eef0a62c4afa46d222e39c
Klisóstom <klis@pm.me>
2021-04-21 21:55:31 -0300

    test(desafio_cinco):
    Pequenas alteração.

--- osProgramadores/desafio_cinco/tests/QuestaoTres/QuestaoTresTest.php
+++ osProgramadores/desafio_cinco/tests/QuestaoTres/QuestaoTresTest.php
@@ -3,18 +3,28 @@
 namespace App;
 
 use Help;
+use App\SalaryArea;
 use PHPUnit\Framework\TestCase;
 
 define('__ROOT__', dirname(dirname(__FILE__)));
+require_once __ROOT__ . '../../vendor/autoload.php';
 require_once __ROOT__ . '../Help.php';
+require_once __ROOT__ . '../../src/SalaryArea.php';
 
 class QuestaoTresTest extends TestCase
 {
     use Help;
 
-    public function test_test(): void
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->salary = new SalaryArea();
+    }
+
+    public function test_least_employees_by_area(): void
+    {
+        $numEmployees = $salaries = $this->salary->print_questao_tres();
+
+        $this->assertIsString($numEmployees);
     }
 }
 
